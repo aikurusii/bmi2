7395997ee057664170e46972f5d7738dfd531203
Omori <s.omori.sys22@morijyobi.ac.jp>
2022-11-08 13:31:22 +0900

    二人分に修正

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 	 Scanner scan = new Scanner(System.in);
+	 for(int i=0; i<2;i++){
                 System.out.println("身長");
                 double height = scan.nextDouble();
                 System.out.println("体重");
@@ -10,7 +11,7 @@ public class Main {
 		double height2 = height / 100.0;
                 double BMI = weight / Math.pow(height,2);
                 System.out.println((double)Math.round(BMI * 100)/100);
-	 
+	 }
                 scan.close();
 		
 	}
